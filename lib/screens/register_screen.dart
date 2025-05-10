6c144a8a968d379ab0ac342a040a521480ebdd3f
Peter Obiechina <obiechinapeter2@gmail.com>
2025-05-10 08:37:40 +0100

    Implemented auth for register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,7 @@
+import 'package:demo/providers/auth_provider.dart';
 import 'package:demo/widgets/shadowed_container.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -9,8 +11,38 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
+  void _register(BuildContext context) async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    try {
+      await authProvider.register(
+        _emailController.text.trim(),
+        _passwordController.text.trim(),
+      );
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Registration successful!')),
+        );
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(e.toString())));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -22,7 +54,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   const SizedBox(height: 24.0),
-                  Text('Sign up with email'),
+                  const Text('Sign up with email'),
                   const SizedBox(height: 24.0),
                   Container(
                     padding: const EdgeInsets.all(20.0),
@@ -32,34 +64,44 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                     child: Icon(
                       Icons.person,
-                      size:
-                          Theme.of(context).textTheme.headlineLarge?.fontSize,
+                      size: Theme.of(context).textTheme.headlineLarge?.fontSize,
                       color: Theme.of(context).colorScheme.onSecondary,
                     ),
                   ),
                   const SizedBox(height: 24.0),
                   ShadowedContainer(
                     child: TextFormField(
+                      controller: _nameController,
                       keyboardType: TextInputType.name,
-                      decoration: InputDecoration(labelText: 'Name'),
+                      decoration: const InputDecoration(labelText: 'Name'),
                     ),
                   ),
                   const SizedBox(height: 16.0),
                   ShadowedContainer(
                     child: TextFormField(
+                      controller: _emailController,
                       keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(labelText: 'Email'),
+                      decoration: const InputDecoration(labelText: 'Email'),
                     ),
                   ),
                   const SizedBox(height: 16.0),
                   ShadowedContainer(
                     child: TextFormField(
-                      obscureText: true,
+                      controller: _passwordController,
+                      obscureText: !_isPasswordVisible,
                       decoration: InputDecoration(
                         labelText: 'Password',
                         suffixIcon: IconButton(
-                          icon: Icon(Icons.visibility),
-                          onPressed: () {},
+                          icon: Icon(
+                            _isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
                         ),
                       ),
                     ),
@@ -67,13 +109,33 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   const SizedBox(height: 40.0),
                   ShadowedContainer(
                     child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Sign Up'),
+                      onPressed:
+                          authProvider.isLoading
+                              ? null
+                              : () => _register(context),
+                      child:
+                          authProvider.isLoading
+                          ? SizedBox(
+                              height: 20,
+                              width: 20,
+                              child: CircularProgressIndicator(
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            )
+                              : const Text('Sign Up'),
                     ),
                   ),
                   const SizedBox(height: 40.0),
-                  Text('Already have an account?', textAlign: TextAlign.center),
-                  TextButton(onPressed: () {}, child: const Text('Login')),
+                  const Text(
+                    'Already have an account?',
+                    textAlign: TextAlign.center,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Login'),
+                  ),
                 ],
               ),
             ),
