6d82f6d2c05c37e9fa7215fcbf4f1c4570ff42f8
Peter Obiechina <obiechinapeter2@gmail.com>
2025-05-10 09:01:16 +0100

    Added logout button and functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:demo/constants/app_images.dart';
+import 'package:demo/providers/auth_provider.dart';
+import 'package:demo/screens/login_screen.dart';
+import 'package:demo/widgets/shadowed_container.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -18,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Users', style: TextTheme.of(context).titleMedium),
+              Text('Users', style: Theme.of(context).textTheme.titleMedium),
               const SizedBox(height: 16.0),
               Expanded(
                 child: ListView.separated(
@@ -35,6 +41,37 @@ class _HomeScreenState extends State<HomeScreen> {
                       title: Text('Name'),
                     );
                   },
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ShadowedContainer(
+                  child: ElevatedButton(
+                    onPressed:
+                        authProvider.isLoading
+                            ? null
+                            : () async {
+                              await authProvider.logout();
+                              if (!context.mounted) return;
+
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => const LoginScreen(),
+                                ),
+                              );
+                            },
+                    child:
+                        authProvider.isLoading
+                            ? SizedBox(
+                              height: 20,
+                              width: 20,
+                              child: CircularProgressIndicator(
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            )
+                            : const Text('Logout'),
+                  ),
                 ),
               ),
             ],
